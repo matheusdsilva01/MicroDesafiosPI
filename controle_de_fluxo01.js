f2501c4a265465cd79baab6911e76fef1b701ad9
Matheus <ms25022003@gmail.com>
2021-06-17 17:59:00 -0300

    limpeza da pasta raiz

--- controle_de_fluxo01.js
+++
@@ -1,7 +0,0 @@
-/* Exercicio "Meu primeiro if" */
-var status = true
-if (status == true){
-    console.log("O valor é verdadeiro")
-} else{
-    console.log("O valor é falso")
-}
--- controle_de_fluxo02.js
+++
@@ -1,8 +0,0 @@
-/* Exercicio "Igualdade, if e else" */
-var linguagem = "javascript"
-
-if (linguagem == "javascript") {
-    console.log ("Estou aprendendo")
-} else {
-    console.log ("Aprenderei mais tarde")
-}
