b264647d4e6b514bda961fe5c9b5455ecbccc951
Matheus da Silva Souza <ms25022003@gmail.com>
2021-07-06 10:01:53 -0300

    Envio da atividade referente a aulas do dia 06.07

---
+++ Métodos de Arrays avaçados/atividade.js
@@ -0,0 +1,36 @@
+// Resoluções comentadas, desculpe o número de comentários de comentários <3
+
+// part.01
+/* var numbers = [2,4,6,8,10,12,14,16,18,20]
+var impares = numbers.map(function(){
+    return [1,3,5,7,9]
+});
+
+console.log(impares) */
+
+
+// part.02
+/* var nomes =["Bruno","Maria","Pedro","Gabriel","Maria"]
+var filtro = nomes.filter(function(){
+    return nomes = "Maria"
+})
+console.log (filtro) */
+
+
+// part.03
+/* var algorismos= [1,5,47,81,2,4]
+var resultado = algorismos.reduce(
+    function(acumulador,numero){
+        return acumulador +'-'+numero
+    }
+)
+console.log(resultado) */
+
+
+// part.04
+/* /var animais=["Cachorro","Gato","Papagaio","Cavalo"]
+var newNome= animais.forEach(
+    function (animais){
+        console.log("O animal é " + animais)
+    }
+) */
